eb7d08b3cd3323a9cdebfcd9b858c8ff07b940e0
Kite <Kite@Ricky-Pc>
2016-08-26 17:17:27 -0500

    Tercer commit

--- RickyPilayIngDeSoft2/src/rickypilayingdesoft2/RickyPilayIngDeSoft2.java
+++ RickyPilayIngDeSoft2/src/rickypilayingdesoft2/RickyPilayIngDeSoft2.java
@@ -5,6 +5,8 @@
  */
 package rickypilayingdesoft2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Kite
@@ -20,6 +22,16 @@ public class RickyPilayIngDeSoft2 {
         System.out.println("1.- Sumar\n"
                          + "2.- Restar\n"
                          + "3.- Multiplicar");
+        Operador op = new Operador();
+        Scanner ingreso = new Scanner(System.in);
+        op.setOpcion(ingreso.nextInt());
+        System.out.println("Ingrese el primer valor: ");
+        op.setValor1(ingreso.nextInt());
+        System.out.println("Ingrese el segundo valor: ");
+        op.setValor2(ingreso.nextInt());
+        
+        
+        
     }
     
 }
