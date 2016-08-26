f47433f041b4e9c3b68cce670f6da8d4d011fad5
Kite <Kite@Ricky-Pc>
2016-08-26 16:58:33 -0500

    Primer commit

---
+++ RickyPilayIngDeSoft2/src/rickypilayingdesoft2/RickyPilayIngDeSoft2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rickypilayingdesoft2;
+
+/**
+ *
+ * @author Kite
+ */
+public class RickyPilayIngDeSoft2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("--------Menú-------------");
+        System.out.println("1.- Sumar\n"
+                         + "2.- Restar\n"
+                         + "3.- Multiplicar");
+    }
+    
+}
