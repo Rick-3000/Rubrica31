3126193ad20de324771773e7c567733b0a27db55
Kite <Kite@Ricky-Pc>
2016-08-26 17:19:22 -0500

    Tercer commit

--- RickyPilayIngDeSoft2/src/rickypilayingdesoft2/Operador.java
+++ RickyPilayIngDeSoft2/src/rickypilayingdesoft2/Operador.java
@@ -11,8 +11,53 @@ package rickypilayingdesoft2;
  */
 public class Operador {
     
-    public int valor1;
-    public int valor2;
-    public int opcion;
+    private int valor1;
+    private int valor2;
+    private int opcion;
+
+    /**
+     * @return the valor1
+     */
+    public int getValor1() {
+        return valor1;
+    }
+
+    /**
+     * @param valor1 the valor1 to set
+     */
+    public void setValor1(int valor1) {
+        this.valor1 = valor1;
+    }
+
+    /**
+     * @return the valor2
+     */
+    public int getValor2() {
+        return valor2;
+    }
+
+    /**
+     * @param valor2 the valor2 to set
+     */
+    public void setValor2(int valor2) {
+        this.valor2 = valor2;
+    }
+
+    /**
+     * @return the opcion
+     */
+    public int getOpcion() {
+        return opcion;
+    }
+
+    /**
+     * @param opcion the opcion to set
+     */
+    public void setOpcion(int opcion) {
+        this.opcion = opcion;
+    }
+    
+    
+    
     
 }
